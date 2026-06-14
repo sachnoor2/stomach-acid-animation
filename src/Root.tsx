0f5c5e731fe0c0109070f281b0c3907682db329a
Rahul Sachdeva <sachnoor2@gmail.com>
2026-06-14 22:00:16 +0530

    Rebuild: src/Root.tsx

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,1 +1,20 @@
-import React from 'react'; import { Composition } from 'remotion'; import { StomachAcidViral } from './StomachAcidViral'; export const RemotionRoot: React.FC = () => { return ( <Composition id='StomachAcid' component={StomachAcidViral} durationInFrames={2400} fps={60} width={1080} height={1920} /> ); };
+import { registerRoot } from 'remotion';
+import { Composition } from 'remotion';
+import { StomachAcidViral } from './StomachAcidViral';
+
+export const RemotionRoot: React.FC = () => {
+  return (
+    <>
+      <Composition
+        id="StomachAcid"
+        component={StomachAcidViral}
+        durationInFrames={450} // 7.5 seconds at 60fps
+        fps={60}
+        width={1080}
+        height={1920}
+      />
+    </>
+  );
+};
+
+registerRoot(RemotionRoot);
