a7079f8d41d025fde8d393117d3218824360ce2d
Rahul Sachdeva <sachnoor2@gmail.com>
2026-06-14 14:18:54 +0530

    Fix Root.tsx import path

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,18 +1,1 @@
-import React from 'react';
-import { Composition } from 'remotion';
-import { StomachAcidViral } from './StomachAcid_VIRAL';
-
-export const RemotionRoot: React.FC = () => {
-	return (
-		<>
-			<Composition
-				id="StomachAcid"
-				component={StomachAcidViral}
-				durationInFrames={2400}
-				fps={60}
-				width={1080}
-				height={1920}
-			/>
-		</>
-	);
-};
+import React from 'react'; import { Composition } from 'remotion'; import { StomachAcidViral } from './StomachAcidViral'; export const RemotionRoot: React.FC = () => { return ( <Composition id='StomachAcid' component={StomachAcidViral} durationInFrames={2400} fps={60} width={1080} height={1920} /> ); };
